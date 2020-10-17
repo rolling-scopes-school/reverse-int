64547f3bb3915a2952b553691f2d7c3166a61f0b
Andrei Tarasevich <eansable@yandex.by>
2020-10-18 00:39:08 +0300

    feat: completed function reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let reverseStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+      reverseStr = `${reverseStr}${str[i]}`
+  }
+
+  return Number(reverseStr);
 }
