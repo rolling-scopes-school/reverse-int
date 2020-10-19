9ac9b1e981c1c2c5f8534795642141a7d848f5f5
kirill1991okt <kirill1991okt@gmail.comt>
2020-10-19 23:20:22 +0300

    complit reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    if (n < 0){
+        let f = Math.abs(n);
+       return Number(f.toString().split('').reverse().join(''))
+  
+    }
+    return Number(n.toString().split('').reverse().join(''))
   
 }
