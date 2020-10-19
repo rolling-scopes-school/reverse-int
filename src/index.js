91a39ed2d44ea01c650702c0a74fce6079b8527c
Andrey Goncharenko <goncharenko.andry@yandex.by>
2020-10-19 23:07:31 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let revStr = n.toString();
+    revStr = revStr.split("").reverse().join("");
+    if(revStr.includes('-')) {
+        revStr = revStr.slice(0, -1);
+    }
+    if(revStr[0] == 0) {
+        revStr = revStr.slice(1);
+    }
+    return revStr;
 }
