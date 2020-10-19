f18f2eafb074b757dced38d68f4400b50efe7da3
Yana Gurinovich <yana.gurinovich@instatsport.com>
2020-10-19 22:14:59 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let newStr ='';
+    for(let i = str.length; i > 0; i--){
+      newStr = newStr + str[i-1];
+    }
+    return parseInt(newStr);
 }
