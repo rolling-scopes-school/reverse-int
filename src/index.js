5cc57aa30ecf0504e17828ab9d12c1fdb3b7c62a
Anatoli Reviakou <an.reviakou@gmil.com>
2021-08-21 20:25:16 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+if (n<0) {n=-n;}
+    
+let result = 0;
+let middle = 0;
+
+    while (n>0) {
+        middle = n % 10;
+        result = result * 10 + middle;
+        n = Math.floor(n / 10); 
+    }
+
+  return result;
 }
+
