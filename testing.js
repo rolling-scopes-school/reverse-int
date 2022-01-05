f6b0f12682aec2380336544314853cc81e7160ce
Styshchyk <svet.tyshchyk@gmail.com>
2022-01-05 17:38:19 +0200

    refactor: change structure of project

--- testing.js
+++
@@ -1,7 +0,0 @@
-module.exports = function reverse(n) {
-    let positive = n > 0;
-    let reversed = n.toString().split("").reverse().join("");
-    return  positive ? parseInt(reversed)  : parseInt(reversed ) * -1;
-
-}
-
