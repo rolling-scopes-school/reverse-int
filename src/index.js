0b5559ea5e327fe6574e0892e11292c4e2133410
Vladislav <vladlipchey05@gmail.com>
2022-01-08 18:56:24 +0200

    feat: add reverseint func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let num1 = n.toString().split('');
+    if(num1[0]==='-')
+    {
+       let j= parseInt(num1);
+       j*=-1;
+       return j;
+    }l
+    let num = num1.reverse().join('');
+    return parseInt(num);
+    
+   
+   
 }
