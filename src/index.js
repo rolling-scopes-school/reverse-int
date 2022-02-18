234da20f6bc98a7d6304192e0de77bcf0792ed8c
Salavat <mail@mail.ru>
2022-02-18 10:36:18 +0500

    feat:Submitting a Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let numAbs = Math.abs(n);
+
+    if (numAbs.toString().length === 2) {
+        let arr = [...numAbs.toString()].reverse();
+        arr.push('0');
+        return Number(arr.join(''));
+    } else {
+        return Number([...numAbs.toString()].reverse().join(''));
+    }
 }
