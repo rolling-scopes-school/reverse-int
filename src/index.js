b4845441fd03a4ce89029557e1cc36b0bf17cfcf
KOSHAK2008 <81808174+KOSHAK2008@users.noreply.github.com>
2022-06-24 15:04:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 module.exports = function reverse (n) {
-  let result = 0;
-    while (n) {
-        result = result * 10 + n % 10;
-        n = Math.floor(n / 10);
-    }
-    return result;
+  return [...`${Math.abs(n)}`].reverse().join('');
 }
