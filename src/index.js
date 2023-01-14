4bb2ff221b891472c9e4042418bd6d9e908ecbb6
Your NameLiaaaaaaaaaaagit config --global user.email you@example.comyllia_vladimirovna@mail.ru <you@example.com>
2023-01-14 20:14:30 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = 
+function reverse(n) {
+    let Massive = String(n).split('');
+
+    if (Massive[0] === '-') {
+        n = Massive.splice(1, (Massive.length -1)).reverse().join('');
+        // console.log(n);
+        return n;
+    } else {
+
+        n = Massive.reverse().join('');
+        return n;
+    }
 }
+
+// console.log(reverse(-1234567));
+
+
+
